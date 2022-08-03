13b1d26cf301487e20fe8ec9d07d617aae43d14d
Kian <shanekiantiu@gmail.com>
2022-08-03 13:29:57 +0800

    Update Favorite Task

--- lib/cubit/task_cubit.dart
+++ lib/cubit/task_cubit.dart
@@ -42,16 +42,22 @@ class TaskCubit extends Cubit<TaskState> {
   }
 
   void addToFavorite(Task task) {
-    // int index = tasks.indexWhere((element) => element.id == task.id);
-    // tasks[index] = task;
-    // emit(state.copyWith(
-    //     id: task.id,
-    //     title: task.title,
-    //     description: task.description,
-    //     isFavorite: task.isFavorite,
-    //     isDone: task.isDone,
-    //     isDeleted: task.isDeleted,
-    //     createdAt: task.createdAt));
+    int index = state.tasks!.indexWhere((element) => element.id == task.id);
+    emit(state.copyWith(
+      tasks: List.from(state.tasks!)
+        ..removeAt(index)
+        ..insert(
+          index,
+          task.copyWith(
+              id: task.id,
+              title: task.title,
+              description: task.description,
+              isFavorite: task.isFavorite,
+              isDone: task.isDone,
+              isDeleted: task.isDeleted,
+              createdAt: task.createdAt),
+        ),
+    ));
   }
 
   void completeTask(Task task) {
